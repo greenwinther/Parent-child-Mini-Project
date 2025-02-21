17d5abf4e3143725d882db0e082c2cbe1a4508d7
VonRiddarn <vonriddarn@gmail.com>
2025-02-21 12:43:49 +0100

    feat: updated showcase

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.scss";
+import PokemonTypeIcon from "./components/PokemonTypeIcon/PokemonTypeIcon";
 import { PokeForm } from "./widgets/PokeForm";
 
 function App() {
 	return (
 		<>
 			<PokeForm />
+			<PokemonTypeIcon type="Bug" />
+			<PokemonTypeIcon type="Fairy" />
 		</>
 	);
 }
